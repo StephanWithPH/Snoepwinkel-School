556af81e9d97adea3f4258d9bb922939eab15c24
Stephan <tollenaarstephan@gmail.com>
2020-02-20 22:53:54 +0100

    Added topbar and footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,11 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{asset('css/app.scss')}}" rel="stylesheet">
 
 </head>
 <body>
+@include('topbar')
 @yield('body')
+@include('footer')
 
+<!-- Google Font Montserrat -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+<!-- FontAwesome -->
+<script src="https://kit.fontawesome.com/c3da043785.js" crossorigin="anonymous"></script>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
---
+++ resources/views/topbar.blade.php
@@ -0,0 +1,62 @@
+<div class="container-fluid">
+    <div class="row bg-dark">
+        <div class="col-12">
+            <div class="text-center pb-1 pt-1">
+                <h6 class="text-white d-inline mr-3"><i class="fab fa-ideal"></i> Veilig betalen met iDeal</h6>
+                <h6 class="mt-1 mb-1 text-white d-inline mr-3"><i class="fas fa-truck"></i> Verzending in 2 werkdagen!</h6>
+                <h6 class="mt-1 mb-1 text-white d-inline"><i class="fab fa-facebook-f"></i> Volg ons op Facebook!</h6>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-6">
+                    <div class="mt-3 mb-3">
+                        <img style="width:300px" src={{ asset('img/logo.png') }}>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <input class="form-control border-primary" placeholder="Typ een zoekwoord...">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row bg-info">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Lolly's</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Dropjes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Zuurstokken</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link">Alle categorieën</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Verzendinformatie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Over ons</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+    </div>
+</div>
