5c50edb7ea3e3914a618f2a56ec5270564d5f396
StephanWithPH <55081044+StephanWithPH@users.noreply.github.com>
2020-02-20 15:05:32 +0100

    Basic setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends ('app')
+
+@section('title', "Home : Snoepshop")
+
+@section('body')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <p class="text-center">Test input</p>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
