9459e2cf3376f6c6180d572a89510f581daf833d
StephanWithPH <55081044+StephanWithPH@users.noreply.github.com>
2020-03-10 12:32:36 +0100

    Added aboutus

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -7,8 +7,17 @@
     @include('flash::message')
     <h3 class="mb-4">{{ __('aboutus.title') }}</h3>
     <div class="row">
-        <div class="col-12 text-center">
-            <p></p>
+        <div class="col-12">
+            <p>Wij zijn Lisa en Rens, oprichters van "De Ouwe Snoeperd".<br/>
+                Een aantal zomers geleden zijn wij gestart met de verkoop van ijs in onze bakfiets. Vele kinderen in de straten van Middelharnis en Dirksland hebben wij verblijd met onze leuke bakfiets en heerlijk ijs. Maar onze bakfiets bleek niet alleen geliefd bij de kinderen, dus kregen we al snel de vraag of wij ook op verzoek kwamen.
+                Wat ooit op een zomerse dag begon als een hobby, bleek een groot succes! En dat is uitgegroeid tot "De Ouwe Snoeperd" die inmiddels al een begrip aan het worden is. De Ouwe Snoeperd is een "catering on wheels" concept en is van alle markten thuis op uw feesten en partijen, braderieën, rommelmarkten, evenementen etc.
+                <br/><br/>
+                Dus heeft "De Ouwe Snoeperd" uw interesse gewekt?.. Neem dan gerust contact met ons op, wij komen graag het ijs/desserts voor u verzorgen! En dat nog altijd voor die hobby prijs.
+                <br/>
+                Bel 06-20305223 of 0629105366
+                <br/>
+                of neem contact op via deze Facebook pagina.
+            </p>
         </div>
     </div>
 </div>
